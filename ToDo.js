cbf26369351796b6bf2498610999da51633e348f
TerhiPine <TEP00006@laurea.fi>
2024-03-06 13:51:57 +0200

    js tasks

--- ToDo.js
+++ ToDo.js
@@ -1,1 +1,63 @@
+// retrieve todo from local storage or initialize an empty array
 
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const toDoInput = document.getElementById("toDoInput");
+const toDoList = document.getElementById("toDoList");
+const toDoCount = document.getElementById("toDoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
+
+// initialize
+
+document.addEventListener("DOMContentLoaded", function () {
+    addButton.addEventListener("click", addTask);
+    toDoInput.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click", deleteAllTasks);
+    displayTasks();
+});
+
+function addTask() {
+    const newTask = toDoInput.value.trim();
+    if (newTask !== "") {
+        todo.push({
+            text: newTask,
+            disabled: false,
+        });
+        saveToLocalStorage();
+        toDoInput.value = "";
+        displayTasks();
+    }
+}
+
+function deleteAllTasks(){
+    console.log("test");
+}
+
+function displayTasks() {
+    toDoList.innerHTML = "";
+    todo.forEach((item, index) => {
+        const p = document.createElement("p");
+        p.innerHTML = `
+            <div class="todo-container">
+                <input type="checkbox" class="todo-checkbox" id="input-${index}" ${item.disabled ? "checked" : ""}>
+                <p id="todo-${index}" class="${item.disabled ? "disabled" : ""
+            }" onclick="editTask(${index})">${item.text}</p>
+            </div>
+        `;
+        p.querySelector(".todo-checkbox").addEventListener("change", () => {
+            toggleTask(index);
+        });
+        toDoList.appendChild(p);
+    });
+}
+
+function saveToLocalStorage(){
+    localStorage.setItem("todo", JSON.stringify(todo));
+}
+
+//seuraavaksi toggleTask, editTask
